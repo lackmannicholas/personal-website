d32e454a77b940f44b16830d4f893247835ff06c
lackmannicholas <lackmannicholas@gmail.com>
2019-10-20 19:26:25 -0700

    adding sets section

--- client/src/components/DataStructures.js
+++ client/src/components/DataStructures.js
@@ -18,6 +18,18 @@ export class DataStructures extends Component {
                         Arrays are one of the most basic data structures there is in the programming world. An array is a group
                         of items that are in a single block of memory where the items can be accessed by an index.
                     </p>
+                    <code>
+                        ["can", "stan", "van", "stan"]
+                    </code>
+                    <h2>
+                        Sets
+                    </h2>
+                    <p>
+                        Sets are a group of items where there cannot be duplicates.
+                    </p>
+                    <samp>
+                        { "can", "stan", "van" }
+                    </samp>
                 </div>
             </div>
         );
