41dd19a9699d8734a2ff102027ad9327f8a44626
lackmannicholas <lackmannicholas@gmail.com>
2023-07-15 20:57:25 -0700

    website 2.0

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var express = require('express');
-var app = express();
+import Blog from Blog
 
-var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname +'/client/build/index.html')); //serves the index.html
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname +'/client/build/index.html');
-  });
-
-app.listen(port); //listens on port 3000 -> http://localhost:3000/
+return <Blog />
