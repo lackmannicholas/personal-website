ed2293baf0627121bf73022a0d87a2529b705388
Nick Lackman <nick.lackman@nicklackmanC02G423JMD6R.local>
2023-07-18 10:02:43 -0700

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Header from './Header';
 import MainFeaturedPost from './MainFeaturedPost';
 import FeaturedPost from './FeaturedPost';
 import Main from './Main';
 import Sidebar from './Sidebar';
-import Footer from './Footer';
-
-const sections = [
-  { title: 'Technology', url: '#' },
-  { title: 'Design', url: '#' },
-  { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' },
-  { title: 'Politics', url: '#' },
-  { title: 'Opinion', url: '#' },
-  { title: 'Science', url: '#' },
-  { title: 'Health', url: '#' },
-  { title: 'Style', url: '#' },
-  { title: 'Travel', url: '#' },
-];
 
 const mainFeaturedPost = {
   title: 'Title of a longer featured blog post',
@@ -80,15 +62,9 @@ const sidebar = {
   ],
 };
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
 export default function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <Header title="Blog" sections={sections} />
+    
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
           <Grid container spacing={4}>
@@ -106,11 +82,6 @@ export default function App() {
             />
           </Grid>
         </main>
-      </Container>
-      <Footer
-        title="Footer"
-        description="Something here to give the footer a purpose!"
-      />
-    </ThemeProvider>
+      
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,96 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Header from './Header';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import Footer from './Footer';
+import ErrorPage from "./routes/ErrorPage";
+import DataStructures from './routes/DataStructures';
+import Algorithms from './routes/Algorithms';
+import SystemsDesign from './routes/SystemsDesign';
+import TeamOrganization from './routes/TeamOrganization';
+import Agile from './routes/Agile';
+import Experience from './routes/Experience';
+import Education from './routes/Education';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/data-structures",
+    element: <DataStructures />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/algorithms",
+    element: <Algorithms />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/systems-design",
+    element: <SystemsDesign />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/team-org",
+    element: <TeamOrganization />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/agile",
+    element: <Agile />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/experience",
+    element: <Experience />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/education",
+    element: <Education />,
+    errorElement: <ErrorPage />
+  }
+]);
+const defaultTheme = createTheme();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const sections = [
+  { title: 'Technology', url: '/' },
+  { title: 'Data Structures', url: '/data-structures' },
+  { title: 'Algorithms', url: '/algorithms' },
+  { title: 'Systems Design', url: '/systems-design' },
+  { title: 'Team Organization', url: '/team-org' },
+  { title: 'Agile', url: '/agile' },
+  { title: 'Experience', url: '/experience' },
+  { title: 'Education', url: '/education' },
+];
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="Blog" sections={sections} />
+        <RouterProvider router={router} />
+      </Container>
+      <Footer
+        title="Footer"
+        description="Something here to give the footer a purpose!"
+      />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
